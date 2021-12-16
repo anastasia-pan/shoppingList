210b0d513a68f8c314d406a8175bc0c313a2626f
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-12-16 14:46:20 +0000

    css altered

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -36,18 +36,24 @@ const ShoppingList = () => {
     setTasks(newArray);
   };
 
+  // const changePosition = (item) => {
+  //   let newArray = [...tasks];
+  //   newArray.push(newArray.splice(newArray.indexOf(item, 1)[0]));
+  //   setTasks(newArray);
+  // };
+
   return (
-    <div>
-      <h1>Add tasks to a to do list</h1>
-      <form onSubmit={addItem}>
+    <div className="wholePage">
+      <h1 className="header">Add tasks to a to do list</h1>
+      <form className="form" onSubmit={addItem}>
         <input value={userInput} onChange={updateValue} />
         <button onClick={addItem}>add an item</button>
       </form>
       <h1>To Do:</h1>
       <ul>
-        <div className="listLine">
-          {tasks.map((task) => (
-            <Task task={task} func={deleted} />
+        <div>
+          {tasks.map((task, index) => (
+            <Task key={index} task={task} func={deleted} />
           ))}
         </div>
       </ul>
@@ -66,7 +72,17 @@ const Task = (props) => {
 
   return (
     <div>
-      <li style={{ color: task.completed ? "red" : "blue" }}>{task.title}</li>
+      <li
+        className="listLine"
+        key="value"
+        style={
+          task.completed
+            ? { textDecoration: "line-through", opacity: "0.6" }
+            : { textDecoration: "none" }
+        }
+      >
+        {task.title}
+      </li>
       <button onClick={props.func}>Delete</button>
       <button onClick={markCompleted}>Mark as done</button>
     </div>
